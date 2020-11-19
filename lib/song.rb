6c6a69f1cc504cc81838f9cd28086302a85370c0
Lucas Montanari <Lmonty2011@gmail.com>
2020-11-19 15:50:29 -0800

    implemented song class to have certain methods

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,57 @@ class Song
     self.class.all << self
   end
 
+  def self.create 
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song 
+  end
+
+  def self.create_by_name(name)
+    song = Song.new_by_name(name)
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if song = Song.find_by_name(name)
+      return song
+    else
+      Song.create_by_name(name) 
+    end
+  end
+
+  def self.alphabetical 
+    @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    song_array = filename.split(/[-.]+/).map(&:strip)
+    song = Song.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = Song.new_from_filename(filename)
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+
 end
